6806429448fdb1d5016ae045725cc1fc54ed9796
ryomanishida <nriyoma@gmail.com>
2021-11-20 13:22:11 +0900

    q3.1-2

--- quest3.sql
+++ quest3.sql
@@ -1,7 +1,37 @@
 1,さくらがフォローしているユーザーの名前を一覧で表示せよ。
+x  5
+SELECT follower_id
+FROM follows
+WHERE followee_id = (
+  SELECT id
+  FROM users
+  WHERE name = "さくら"
+);
+
+SELECT u2.name
+FROM users u1
+JOIN follows f
+ON u1.id = f.follower_id
+JOIN users u2
+ON f.followee_id = u2.id
+WHERE u1.id = 3;
 
 
 2,誰もフォローしていないユーザーの名前を表示せよ。
+X  8
+SELECT u.name
+FROM users u
+JOIN follows f
+ON u.id = f.follower_id
+WHERE f.followee_id is null;
+
+A
+SELECT u.name
+FROM users u
+LEFT JOIN follows f
+ON u.id = f.follower_id
+WHERE f.followee_id IS NULL;
+
 
 
 3,10代、20代、30代といった年代別にフォロー数の平均を表示せよ。
